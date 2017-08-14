635520f69c5ac027b19e40269ffd1263107f585d
Andreas <a.theodorou@live.com>
2017-08-14 18:56:06 +0100

    RC Golden

--- Assets/Scripts/Agent/AgentActionsSelector.cs
+++ Assets/Scripts/Agent/AgentActionsSelector.cs
@@ -39,7 +39,7 @@ public class AgentActionsSelector : MonoBehaviour
         dateBorn = timeDistribution.DaysPassed;
 
         nextStaminaUpdate = timeDistribution.TimeInDay;
-        staminaUpdateTime = timeDistribution.TimeInDay / 1.5f;
+        staminaUpdateTime = timeDistribution.TimeInDay / 1.75f;
 
         SetRandomAge();
         defaultColor = new Color(0, 1, 1, 1);
